b3107b4d445cb704282609335287d649bde9e6a8
Ko-GyeongTae <kokt0203@naver.com>
2021-01-18 22:46:05 +0900

    Edit: create LocalState.js

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,5 +1,10 @@
 import ApolloClient from "apollo-boost";
+import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-    uri: "https://localhost:4000"
-})
+    uri: "https://localhost:4000",
+    clientState: {
+        defaults,
+        resolvers
+    }
+});
