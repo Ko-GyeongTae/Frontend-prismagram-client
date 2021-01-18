995012b83331dadcc9118a0dd58a4e1b262f8773
Ko-GyeongTae <kokt0203@naver.com>
2021-01-18 18:52:50 +0900

    Edit: create Theme

---
+++ src/Styles/Theme.js
@@ -0,0 +1,19 @@
+const boxBorder = "px solid #e6e6e6";
+const borderRadius = "4px";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  redColor: "#ED4956",
+  blueColor: "#3897f0",
+  darkBluColor: "#003569",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `
+    ${borderRadius};
+    ${boxBorder};
+    background-color: white;
+  `
+};
