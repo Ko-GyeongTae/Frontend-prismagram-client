32073354b22155259cf9d48f4ce7d557a3332c13
Ko-GyeongTae <kokt0203@naver.com>
2021-01-19 14:02:02 +0900

    Edit: AuthContainer.js & AuthQueries

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
+import { LOG_IN } from "./AuthQueries";
+import { useMutation } from "react-apollo-hooks";
 
 export default () => {
-  const [action, setAction] = useState("logIn");
-  const username = useInput("");
-  const firstName = useInput("");
-  const lastName = useInput("");
-  const email = useInput("");
-
-  const onLogin = (e) => {
-      e.preventDefault();
-  }
+    const [action, setAction] = useState("logIn");
+    const username = useInput("");
+    const firstName = useInput("");
+    const lastName = useInput("");
+    const email = useInput("");
+    const [requestSecret] = useMutation(LOG_IN, {variables: {email: email.value}});
+    
+    const onLogin = (e) => {
+    e.preventDefault();
+    if(email !== ""){
+        requestSecret();
+    }
+  };
 
   return (
     <AuthPresenter
