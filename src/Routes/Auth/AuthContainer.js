e6e9470647b9cb2ce5b0928f2250819de1fe497b
Ko-GyeongTae <kokt0203@naver.com>
2021-01-19 22:34:22 +0900

    Edit: create Login

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
-import { CREATE_ACCOUNT, LOG_IN } from "./AuthQueries";
+import { CONFIRM_SECRET, CREATE_ACCOUNT, LOCAL_LOG_IN, LOG_IN } from "./AuthQueries";
 import { useMutation } from "react-apollo-hooks";
 import { toast } from "react-toastify";
 
@@ -23,6 +23,14 @@ export default () => {
       lastName: lastName.value
     }
   });
+  const [confirmSecretMutation] = useMutation(CONFIRM_SECRET, {
+      variables:{
+          email:email.value,
+          secret:secret.value
+      }
+  });
+  const [localLogInMutation] = useMutation(LOCAL_LOG_IN);
+
   const onSubmit = async e => {
     e.preventDefault();
     if (action === "logIn") {
@@ -63,6 +71,21 @@ export default () => {
       } else {
         toast.error("All field are required");
       }
+    } else if(action === "confirm"){
+        if(secret.value !== ""){
+            try{
+                const { data:{confirmSecret:token} } = await confirmSecretMutation();
+                if(token !== "" && token !== undefined){
+                    localLogInMutation({
+                        variables:{
+                            token
+                        }
+                    });
+                }
+            } catch {
+                toast.error("Can't confirm secret");
+            }
+        }
     }
   };
 
