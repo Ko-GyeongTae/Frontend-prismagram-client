d62c45cb0bed88f1d2682abb1985f431324abbb1
Ko-GyeongTae <kokt0203@naver.com>
2021-01-20 11:32:58 +0900

    Edit: Feed.js

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,32 @@
-export default () => "Feed";
+import React from "react";
+import { gql } from "apollo-boost";
+
+const FEED_QUERY = gql`
+    {
+        seeFeed{
+            id
+            location
+            caption
+            user{
+                id
+                avatar
+                username
+            }
+            files{
+                id
+                url
+            }
+            likeCount
+            isLiked
+            comments{
+                id
+                text
+                user{
+                    id
+                    avatar
+                    username
+                }
+            }
+        }
+    }
+`;
