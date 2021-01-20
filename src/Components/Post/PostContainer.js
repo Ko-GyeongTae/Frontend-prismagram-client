ebbaa86c93460a89129aa49d6ea56219e1fe8371
Ko-GyeongTae <kokt0203@naver.com>
2021-01-20 13:46:04 +0900

    Edit: editing

---
+++ src/Components/Post/PostContainer.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import PropTypes from "proptypes";
+import useInput from "../../Hooks/useInput";
+import PostPresenter from "./PostPresenter";
+
+const PostContainer = ({id, user, files}) => {}
+
+PostContainer.propTyeps = {
+
+}
+
+export default PostContainer;
--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
 import Loader from "../Components/Loader";
+import Post from "../Components/Post";
 
 const FEED_QUERY = gql`
     {
@@ -45,7 +46,23 @@ export default () => {
     const { data, loading } = useQuery(FEED_QUERY);
     return (
         <Wrapper>
-            {loading && <Loader />}{!loading && data && data.seeFeed && "We have photos"}
+            {loading && <Loader />}
+            {!loading && 
+                data && 
+                data.seeFeed && 
+                data.seeFeed.map(post => (
+                    <Post 
+                        key={post.id} 
+                        id={post.id} 
+                        user={post.user} 
+                        files={files}
+                        likeCount={likeCount}
+                        isLiked={isLiked}
+                        comments={comments}
+                        createdAt={createdAt}
+                    />
+                ))
+            }
         </Wrapper>
     );
 }
