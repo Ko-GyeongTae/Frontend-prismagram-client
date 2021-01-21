9274b7aff418489fa8602535833f0fbf97968ca3
Ko-GyeongTae <kokt0203@naver.com>
2021-01-21 17:33:36 +0900

    Add: index.js

--- src/Routes/Search.js
+++ src/Routes/Search.js
@@ -1,1 +1,19 @@
-export default () => "Search";
+import React from "react";
+import { withRouter } from "react-router-dom";
+import styled from "styled-components";
+import FatText from "../Components/FatText";
+
+const Wrapper = styled.div`
+    height: 50vh;
+    text-align: center;
+`;
+
+export default withRouter(({location: {search}}) => {
+    const searchTerm = search.split("=")[1];
+    console.log(searchTerm);
+    return (
+        <Wrapper>
+            {searchTerm === "" && <FatText text={"Search for something"} />}
+        </Wrapper>
+    );
+});
