e88f9ab3dcd887be56189bdaa47ea87edf15f0fa
Ko-GyeongTae <kokt0203@naver.com>
2021-01-21 19:06:45 +0900

    Edit: SearchPresenter.js

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -5,15 +5,51 @@ import FatText from "../../Components/FatText";
 
 const Wrapper = styled.div`
   height: 50vh;
-  text-align: center;
 `;
 
-const SearchPresenter = ({ searchTerm }) => (
-    console.log(searchTerm),
-  <Wrapper>
-    {searchTerm === undefined && <FatText text={"Search for something"} />}
-  </Wrapper>
-);
+const Section = styled.div``;
+
+const SearchPresenter = ({ searchTerm, loading, data }) => {
+  if (searchTerm === undefined) {
+    return (
+      <Wrapper>
+        <FatText text="Search for something" />
+      </Wrapper>
+    );
+  } else if (loading === true) {
+    return (
+      <Wrapper>
+        <Loader />
+      </Wrapper>
+    );
+  } else if (data && data.searchUser && data.searchPost) {
+    return (
+      <Wrapper>
+        <Section>
+          {data.searchUser.length === 0 ? (
+            <FatText text="No Users Found" />
+          ) : (
+            data.searchUser.map((user) => (
+              <UserCard
+                username={user.username}
+                isFollowing={user.isFollowing}
+                url={user.url}
+                isSelf={user.isSelf}
+              />
+            ))
+          )}
+        </Section>
+        <Section>
+          {data.searchPost.length === 0 ? (
+            <FatText text="No Posts Found" />
+          ) : (
+            data.searchPost.map((post) => null)
+          )}
+        </Section>
+      </Wrapper>
+    );
+  }
+};
 
 SearchPresenter.propTypes = {
   searchTerm: PropTypes.string,
