969ccceb5af0950e93a77d0f3ec32aa333f1445d
Ko-GyeongTae <kokt0203@naver.com>
2021-01-23 00:35:44 +0900

    Edit: log Out func

--- src/Routes/Profile/ProfileContainer.js
+++ src/Routes/Profile/ProfileContainer.js
@@ -3,7 +3,6 @@ import { gql } from "apollo-boost";
 import { withRouter} from "react-router-dom";
 import { useMutation, useQuery } from "react-apollo-hooks";
 import ProfilePresenter from "./ProfilePresenter";
-import { LOG_IN } from "../Auth/AuthQueries";
 
 const GET_USER = gql`
   query seeUser($username: String!) {
@@ -43,7 +42,7 @@ export default withRouter(
     },
   }) => {
     const { data, loading } = useQuery(GET_USER, { variables: { username } });
-    const logOut = useMutation(LOG_IN);
+    const logOut = useMutation(LOG_OUT);
     return <ProfilePresenter loading={loading} logOut={logOut} data={data} />;
   }
 );
